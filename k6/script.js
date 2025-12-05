2c7547b6f4a1a7e780578597d6b0c83739b6cbbe
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2025-12-05 20:24:52 -0300

    conserta bugs e testes iniciais básicos do k6

--- k6/script.js
+++ k6/script.js
@@ -26,44 +26,31 @@ const SCENARIOS = {
     },
 };
 
-// Lógica para selecionar o cenário via variável de ambiente
-// Ex: k6 run -e SCENARIO=smoke script.js
 const currentScenario = __ENV.SCENARIO || 'all';
 
 let selectedScenarios = {};
 
 if (currentScenario === 'all') {
-    // Se for 'all', configura para rodar um após o outro usando startTime
     selectedScenarios = {
         smoke_test: { ...SCENARIOS.smoke, startTime: '0s' },
-        load_test: { ...SCENARIOS.load, startTime: '35s' }, // 30s do smoke + 5s folga
-        spike_test: { ...SCENARIOS.spike, startTime: '2m40s' }, // Tempo acumulado anterior
+        load_test: { ...SCENARIOS.load, startTime: '35s' },
+        spike_test: { ...SCENARIOS.spike, startTime: '2m40s' },
     };
 } else if (SCENARIOS[currentScenario]) {
-    // Se escolheu um específico, roda apenas ele
     selectedScenarios = {
         [currentScenario]: SCENARIOS[currentScenario],
     };
 } else {
-    throw new Error(`Cenario '${currentScenario}' não encontrado. Use: smoke, load, spike ou não passe nada.`);
+    throw new Error(`Cenário '${currentScenario}' não existe.`);
 }
+
+export const options = {
+    scenarios: selectedScenarios,
+};
 
 const BASE_URL = 'http://localhost:8080/buyTicket?flight=1234&day=23&user=167&ft=true';
 
 export default function () {
-
-    /*
-    const payload = generateRandomPayload();
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'K6-LoadTest/1.0',
-        },
-    };
-
-     */
-
-    //const res = http.post(BASE_URL, payload, params);
     const res = http.post(BASE_URL);
 
     console.log('STATUS:', res.status);
